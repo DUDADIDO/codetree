503f54b55749f795dd9c0965af5424754a15f1de
DUDADIDO <82928491+DUDADIDO@users.noreply.github.com>
2026-03-04 17:22:32 +0900

    Title: 안전 지대, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260304/안전 지대/comfort-zone.java
+++ 260304/안전 지대/comfort-zone.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class Main {
@@ -10,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int maxHeight = Integer.MIN_VALUE;
-        int maxCnt = Integer.MIN_VALUE;
+        int maxCnt = 0;
         int maxIdx = 0;
         
         n = sc.nextInt();
